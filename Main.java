d1c7f1ac288fba2511f3ee15ddf9c7b4976037ae
AlexandreCJ <acjurka@gmail.com>
2023-08-17 09:03:37 -0300

    deleted file

--- Main.java
+++
@@ -1,8 +0,0 @@
-
-public class Main {
-    public static void main(String[] args) throws Exception {
-        Arquivo arq = new Arquivo("ListaAnime.csv", "animeBin.bin");
-        arq.CsvToByte();
-        
-    }
-}
